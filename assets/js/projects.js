4b0bcfc44f7860bf1db7e960a574e7b28c1b2a5c
annechoww <anne.chow123@hotmail.com>
2024-09-18 19:51:29 -0400

    Fix projects bug

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -59,7 +59,7 @@ const projects = [
   }
   
   function displayProjects() {
-    const recentProjectsDiv = document.getElementById("projects");
+    const recentProjectsDiv = document.getElementById("recent-projects");
     recentProjectsDiv.innerHTML = ""; // Clear the section before appending
   
     for (let i = 0; i < displayedProjects; i++) {
